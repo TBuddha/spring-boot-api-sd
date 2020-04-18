364c6326da418a078ba754cca2a79a0038cbb2cb
zhout <zhoutao@yunjiacloud.com>
2020-04-18 18:40:49 +0800

    在基础版本上加入swagger2支持！

--- src/main/java/com/company/project/configurer/WebMvcConfigurer.java
+++ src/main/java/com/company/project/configurer/WebMvcConfigurer.java
@@ -6,7 +6,6 @@ import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.company.project.core.Result;
 import com.company.project.core.ResultCode;
-import com.company.project.core.ServiceException;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -15,22 +14,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -54,8 +49,8 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     // 按需配置，更多参考FastJson文档哈
 
     converter.setFastJsonConfig(config);
-    converter.setDefaultCharset(Charset.forName("UTF-8"));
-    converter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON_UTF8));
+    converter.setDefaultCharset(StandardCharsets.UTF_8);
+    converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON_UTF8));
     converters.add(converter);
   }
 
@@ -63,46 +58,18 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
   @Override
   public void configureHandlerExceptionResolvers(
       List<HandlerExceptionResolver> exceptionResolvers) {
-    exceptionResolvers.add(
-        new HandlerExceptionResolver() {
-          public ModelAndView resolveException(
-              HttpServletRequest request,
-              HttpServletResponse response,
-              Object handler,
-              Exception e) {
-            Result result = new Result();
-            if (e instanceof ServiceException) { // 业务失败的异常，如“账号或密码错误”
-              result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
-              logger.info(e.getMessage());
-            } else if (e instanceof NoHandlerFoundException) {
-              result
-                  .setCode(ResultCode.NOT_FOUND)
-                  .setMessage("接口 [" + request.getRequestURI() + "] 不存在");
-            } else if (e instanceof ServletException) {
-              result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
-            } else {
-              result
-                  .setCode(ResultCode.INTERNAL_SERVER_ERROR)
-                  .setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
-              String message;
-              if (handler instanceof HandlerMethod) {
-                HandlerMethod handlerMethod = (HandlerMethod) handler;
-                message =
-                    String.format(
-                        "接口 [%s] 出现异常，方法：%s.%s，异常摘要：%s",
-                        request.getRequestURI(),
-                        handlerMethod.getBean().getClass().getName(),
-                        handlerMethod.getMethod().getName(),
-                        e.getMessage());
-              } else {
-                message = e.getMessage();
-              }
-              logger.error(message, e);
-            }
-            responseResult(response, result);
-            return new ModelAndView();
-          }
-        });
+    exceptionResolvers.add(new GlobalHandlerExceptionResolver());
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry
+        .addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 
   // 解决跨域问题
@@ -199,7 +166,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
       ip = request.getRemoteAddr();
     }
     // 如果是多级代理，那么取第一个ip为客户端ip
-    if (ip != null && ip.indexOf(",") != -1) {
+    if (ip != null && ip.contains(",")) {
       ip = ip.substring(0, ip.indexOf(",")).trim();
     }
 
